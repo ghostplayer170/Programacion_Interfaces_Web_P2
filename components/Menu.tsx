d34f3d7f97f66fe5b0a81a560a71570b72038793
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2024-03-31 13:06:06 +0200

    first commit 3

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -4,18 +4,26 @@ const Menu: FunctionComponent = () => {
   return (
     <>
       <div class="menu">
-        <h1>LA SOCIEDAD DE LA JUSTICIA DE LOS MONDONGOS</h1>
-        <ul>
-          <li>
-            <a href="/">Heros</a>
-          </li>
-          <li>
-            <a href="/search">Search</a>
-          </li>
-          <li>
-            <a href="/create">Create</a>
-          </li>
-        </ul>
+        <div class="gif-menu">
+          <img src="/images/heros2.gif" alt="Hero" class="gif-img"/>
+        </div>
+        <div class="menu-title-options">
+          <h1>LA SOCIEDAD DE LA JUSTICIA DE LOS MONDONGOS</h1>
+          <ul>
+            <li>
+              <a href="/">Heros</a>
+            </li>
+            <li>
+              <a href="/search">Search</a>
+            </li>
+            <li>
+              <a href="/create">Create</a>
+            </li>
+          </ul>
+        </div>
+        <div class="gif-menu">
+          <img src="/images/heros1.gif" alt="Hero" class="gif-img" />
+        </div>
       </div>
     </>
   );
--- islands/DeleteHero.tsx
+++ islands/DeleteHero.tsx
@@ -12,9 +12,7 @@ const DeleteHero: FunctionComponent<Data> = (props) => {
   const name = props.hero;
   const enable = props.enable;
   const currentPath = props.currentPath;
-
   const [isModalOpen, setModal] = useState<boolean>(false);
-  const [deleted, setDeleted] = useState<boolean>(false);
 
   const openModal = () => setModal(true);
   const closeModal = () => setModal(false);
@@ -24,8 +22,8 @@ const DeleteHero: FunctionComponent<Data> = (props) => {
       {enable && <button onClick={openModal}>Delete</button>}
       {isModalOpen && (
         <dialog open id="modal-delete" class="modal">
-          <p>Hero to delete is {name}</p>
-          <p>Creator of the hero</p>
+          <p>Hero <strong>{name}</strong> will be eliminated !!</p>
+          <p>To delete writes its creator:</p>
           <form method="POST" action={`/delete`}>
             <input
               type="text"
@@ -37,7 +35,6 @@ const DeleteHero: FunctionComponent<Data> = (props) => {
             <div>
               <button type={"submit"}>Delete Hero</button>
               <button onClick={closeModal}>Close</button>
-              {deleted && <p>Hero deleted successfully!</p>}
             </div>
           </form>
         </dialog>
